61c1ad2f27a925f5f47ca1f2a11192bdd64a2238
Lei Zhang <antiagainst@gmail.com>
2014-07-14 21:35:11 -0400

    Special tweak for memory allocation. When klee tries to allocate a huge chunk of memory, return NULL pointer directly.

--- lib/Core/MemoryManager.cpp
+++ lib/Core/MemoryManager.cpp
@@ -36,8 +36,11 @@ MemoryManager::~MemoryManager() {
 MemoryObject *MemoryManager::allocate(uint64_t size, bool isLocal, 
                                       bool isGlobal,
                                       const llvm::Value *allocSite) {
-  if (size>10*1024*1024)
+  if (size>10*1024*1024) {
     klee_warning_once(0, "Large alloc: %u bytes.  KLEE may run out of memory.", (unsigned) size);
+    return 0;
+  }
+
   
   uint64_t address = (uint64_t) (unsigned long) malloc((unsigned) size);
   if (!address)
